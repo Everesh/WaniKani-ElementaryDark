c05068b365d0afab4a84162f13dbd1cca6c7c1f1
Everesh <everesh@proton.me>
2025-04-21 21:47:29 +0200

    Add make.sh metadata generation

--- make.sh
+++ make.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-sass WaniKani-ElementaryDark-Reborn.scss test.css
-cat test.css | xclip -sel clip
+SASS_INPUT="WaniKani-ElementaryDark.scss"
+TMP_STEP="tmp.css"
+OUTPUT="WaniKani-ElementaryDark.css"
+
+trap 'rm -f "$TMP_STEP"*' EXIT
+
+sass "$SASS_INPUT" "$TMP_STEP"
+
+echo "/* ==UserStyle==
+@name         WaniKani Elementary Dark
+@namespace    github.com/openstyles/stylus
+@version      3.0.0
+@license      MIT
+@description  Dark theme for the WaniKani domain
+@author       Everesh
+@homepageURL  https://github.com/Everesh/WaniKani-ElementaryDark
+@supportURL   https://github.com/Everesh/WaniKani-ElementaryDark/issues
+==/UserStyle== */
+
+@-moz-document domain("www.wanikani.com") {" > "$OUTPUT"
+
+sed 's/^/  /' "$TMP_STEP" >> "$OUTPUT"
+
+echo "}" >> "$OUTPUT"
+
+rm "$TMP_STEP"
+
+cat WaniKani-ElementaryDark.css | xclip -sel clip
