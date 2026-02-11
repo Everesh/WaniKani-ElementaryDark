bf3f9d67b243109cb714798f546b196c4f7378e0
Everesh <everesh@proton.me>
2026-02-11 21:50:01 +0100

    add wkof integration toggle

--- make.sh
+++ make.sh
@@ -21,7 +21,15 @@ if ! command -v sass &> /dev/null; then
     exit 1
 fi
 
-PROCESSED_CSS=$(sass "$INPUT" | sed 's/^/  /')
+COMPILED_CSS=$(sass "$INPUT")
+
+MAIN_CSS=$(echo "$COMPILED_CSS" | awk '/WKOF_START/{f=1} !f; /WKOF_END/{f=0}' | sed 's/^/  /')
+
+WKOF_CSS=$(echo "$COMPILED_CSS" | awk '/WKOF_START/{f=1; next} /WKOF_END/{f=0} f')
+
+WKOF_ADAPTIVE=$(echo "$WKOF_CSS" | sed 's/^/  /')
+
+WKOF_ENFORCE=$(echo "$WKOF_CSS" | sed 's/\([^!]\);$/\1 !important;/' | sed 's/^/  /')
 
 VERSION_VAL=$(cat "$VERSION_FILE")
 
@@ -35,10 +43,22 @@ cat <<EOF >"$OUTPUT"
 @author       Everesh
 @homepageURL  https://github.com/Everesh/WaniKani-ElementaryDark
 @supportURL   https://github.com/Everesh/WaniKani-ElementaryDark/issues
+
+@advanced     dropdown wkof_mode "Open Framework integration" {
+    wkof_adaptive "Adaptive" <<<EOT
+${WKOF_ADAPTIVE}
+    EOT;
+    wkof_strict "Strict" <<<EOT
+${WKOF_ENFORCE}
+    EOT;
+    wkof_none "None" <<<EOT
+    EOT;
+}
 ==/UserStyle== */
 
 @-moz-document domain("www.wanikani.com") {
-${PROCESSED_CSS}
+${MAIN_CSS}
+  /*[[wkof_mode]]*/
 }
 EOF
 
